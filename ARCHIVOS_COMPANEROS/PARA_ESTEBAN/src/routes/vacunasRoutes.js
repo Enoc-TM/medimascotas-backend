a9b7046f1680b453313577aa04b217d44c2d718c
Enoc-TM <enoc88419@gmail.com>
2026-09-23 03:47:10 +0000

    Eliminar carpeta y archivo definitivamente

--- ARCHIVOS_COMPANEROS/PARA_ESTEBAN/src/routes/vacunasRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const vacunasController = require('../controllers/vacunasController');
-
-router.post('/', vacunasController.create);
-router.get('/', vacunasController.getAll);
-router.get('/mascota/:mascotaId', vacunasController.getByMascota);
-
-module.exports = router;
--- ARCHIVOS_COMPANEROS/PARA_SANTIAGO/src/routes/historialesRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const historialesController = require('../controllers/historialesController');
-
-router.get('/:mascotaId', historialesController.getByMascotaId);
-router.put('/:mascotaId/antecedentes', historialesController.updateAntecedentes);
-router.get('/:mascotaId/reporte', historialesController.getReporte);
-
-module.exports = router;
